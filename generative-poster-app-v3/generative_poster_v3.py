1ff8b57b9cb0630ddd6e3b3cc000f927d658149d
2320704536 <wxr2320704536@gmail.com>
2025-10-28 13:57:27 +0900

    Update generative_poster_v3.py

--- generative-poster-app-v3/generative_poster_v3.py
+++ generative-poster-app-v3/generative_poster_v3.py
@@ -5,15 +5,14 @@ import matplotlib.pyplot as plt
 import streamlit as st
 import io
 from colorsys import hsv_to_rgb
-
 # ---------------------------------------
 # Page config
 # ---------------------------------------
-st.set_page_config(page_title="🎨 Generative Poster v3", layout="wide")
+st.set_page_config(page_title="Generative Poster v3", layout="wide")
 st.title("Generative Abstract Poster v3")
 st.markdown("**Interactive - Arts & Advanced Big Data**  \nAuto-refresh + Randomize button - Palette & Background themes")
 
-Auto-refresh + Randomize button • Palette & Background themes")
+Auto-refresh + Randomize button  Palette & Background themes")
 
 # ---------------------------------------
 # Palette utilities
